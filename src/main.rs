6083b458049ed459fd9ab522d643c02073371224
uraneko <BOOTHAlessandra@tutanota.com>
2025-12-22 11:07:33 +0100

    clean up dependencies | add auth service with no proper logic yet | add main server event loop

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,108 @@
-fn main() {}
+use pheasant::http::{Method, Protocol, Respond, request::Request, status};
+use pheasant::services::{
+    Server, Socket, bad_request, parse, read_stream, req_buf, resp_write_stream,
+};
+
+mod services;
+use services::lookup;
+
+#[derive(Debug)]
+enum Error {
+    ServerMishap,
+    ServerBroken,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut socket = Socket::builder([127, 10, 10, 1], 6668)
+        .build()
+        .map_err(|_| Error::ServerMishap)?;
+
+    socket.init_message();
+    socket
+        .event_loop(async |this: &mut Socket| {
+            let mut resp = Respond::new(Protocol::Http11, status!(200));
+            while let Ok((mut stream, _)) = read_stream(&this.socket) {
+                resp.clear();
+                // parse req
+                let mut reader = std::io::BufReader::new(&mut stream);
+                let Ok(req_buf) = req_buf(&mut reader) else {
+                    bad_request(&mut resp);
+                    resp_write_stream(&resp, &mut stream, Method::Get)?;
+                    continue;
+                };
+                let req = parse(req_buf);
+                let Ok(req) = req else {
+                    bad_request(&mut resp);
+                    resp_write_stream(&resp, &mut stream, Method::Get)?;
+                    continue;
+                };
+                print_req(&req);
+                let method = req.method();
+
+                // lookup should fetch whole service chains
+                let service = match lookup(&req.path_str()) {
+                    Ok(s) => s,
+                    Err(_err) => {
+                        bad_request(&mut resp);
+                        resp_write_stream(&resp, &mut stream, method)?;
+                        continue;
+                    }
+                };
+                _ = this.service(req, &mut resp, service).await;
+                print_resp(&resp);
+                resp_write_stream(&resp, &mut stream, method)?;
+            }
+
+            Ok(())
+        })
+        .await
+        .map_err(|_| Error::ServerBroken)?;
+
+    Ok(())
+}
+
+fn print_resp(resp: &Respond) {
+    println!(
+        "{} {} {}",
+        resp.proto_cpy().as_str(),
+        resp.status_cpy().code(),
+        resp.status_cpy().text()
+    );
+    println!(
+        "{}",
+        str::from_utf8(resp.headers_ref()).unwrap_or_else(|_| "headers err".into())
+    );
+    println!(
+        "{}",
+        str::from_utf8(resp.body_ref()).unwrap_or_else(|_| "body err".into())
+    );
+    println!("***\n");
+}
+
+fn print_req(req: &Request) {
+    println!(
+        "{} - {} - {:?} - {}",
+        req.method(),
+        req.path_str(),
+        req.query(),
+        req.proto(),
+    );
+    req.headers()
+        .iter()
+        .inspect(|h| {
+            println!(
+                "{} -> {}",
+                str::from_utf8(h.field_ref()).unwrap_or_else(|_| "field err".into()),
+                str::from_utf8(h.value_ref()).unwrap_or_else(|_| "value err".into())
+            )
+        })
+        .count();
+    if let Some(body) = req.body() {
+        println!(
+            "{}",
+            str::from_utf8(body).unwrap_or_else(|_| "body err".into())
+        );
+    }
+    println!("---");
+}
---
+++ src/services.rs
@@ -0,0 +1,157 @@
+use pheasant::http::{ErrorStatus, Header, Method, Respond, err_stt, request::Request};
+use pheasant::services::{
+    Cors, MessageBodyInfo, ReadCookies, Resource, Service, Socket, WriteCookies,
+};
+
+type HttpResult<T> = Result<T, ErrorStatus>;
+
+impl Service<Socket> for Services {
+    async fn serve(&self, socket: &mut Socket, req: Request, resp: &mut Respond) -> HttpResult<()> {
+        match self {
+            Self::Auth => Auth::new(0).run(socket, req, resp).await?,
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum Services {
+    Auth,
+}
+
+#[derive(Debug)]
+enum Auth {
+    Nameless,
+    Traveller,
+    Citizen,
+    Mayor,
+}
+
+impl Auth {
+    fn new(clearance: u8) -> Self {
+        match clearance {
+            0 => Self::Nameless,
+            1 => Self::Traveller,
+            2 => Self::Citizen,
+            4 => Self::Mayor,
+            _ => Self::Nameless,
+        }
+    }
+
+    fn elevate() {}
+
+    fn drop() {}
+}
+
+impl Resource<Socket> for Auth {
+    // gets current auth token
+    async fn get(&self, socket: &mut Socket, req: Request, resp: &mut Respond) -> HttpResult<()> {
+        std::thread::sleep(std::time::Duration::from_millis(4000));
+        let cookies = ReadCookies::from_headers(req.headers()).map_err(|_| err_stt!(400))?;
+        if !cookies.contains(b"tkn") {
+            let mut cookies = WriteCookies::new();
+            // set a traveller token cookie for the recent user
+            cookies
+                .cookie(b"tkn", b"3r2eFE^$TGRE^#EWF")
+                .samesite(0)
+                .secure(true)
+                .partitioned(true)
+                .max_age(183);
+            _ = cookies.write(b"tkn", resp.headers_mut());
+        }
+        Cors::new()
+            .origins(&[
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002",
+            ])
+            .headers(&["content-type", "content-length", "set-cookie"])
+            .credentials(true)
+            .methods(&[Method::Post, Method::Put])
+            .cors_with_cookies(req.headers(), resp.headers_mut())
+            .map_err(|_| err_stt!(403))?;
+
+        let data = format!("{}", 1).into_bytes();
+        resp.body_mut().extend(&data);
+        MessageBodyInfo::new(&data).dump_headers(resp.headers_mut());
+
+        Ok(())
+    }
+
+    async fn options(
+        &self,
+        socket: &mut Socket,
+        req: Request,
+        resp: &mut Respond,
+    ) -> HttpResult<()> {
+        let cookies = ReadCookies::from_headers(req.headers()).map_err(|_| err_stt!(400))?;
+        if !cookies.contains(b"tkn") {
+            let mut cookies = WriteCookies::new();
+            // set a traveller token cookie for the recent user
+            cookies
+                .cookie(b"tkn", b"Traveller")
+                .samesite(0)
+                .secure(true)
+                .partitioned(true)
+                .max_age(183);
+
+            _ = cookies.write(b"tkn", resp.headers_mut());
+        }
+        Cors::new()
+            .origins(&[
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002",
+            ])
+            .header("*")
+            .credentials(true)
+            .methods(&[Method::Post, Method::Put])
+            .cors(req.headers(), resp.headers_mut())
+            .map_err(|_| err_stt!(403))?;
+
+        Ok(())
+    }
+
+    // changes the auth token
+    // elevates or drops user
+    async fn post(&self, socket: &mut Socket, req: Request, resp: &mut Respond) -> HttpResult<()> {
+        Ok(())
+    }
+
+    // creates a new auth token
+    // happens when user opens site
+    async fn put(&self, socket: &mut Socket, req: Request, resp: &mut Respond) -> HttpResult<()> {
+        Ok(())
+    }
+
+    // deletes an auth token
+    async fn delete(
+        &self,
+        socket: &mut Socket,
+        req: Request,
+        resp: &mut Respond,
+    ) -> HttpResult<()> {
+        Ok(())
+    }
+}
+
+pub fn lookup(path: &str) -> HttpResult<Services> {
+    Ok(match path {
+        "/auth/user" => Services::Auth,
+        _ => return err_stt!(?404),
+    })
+}
+
+pub fn route_user_method(method: Method, action: &str) -> HttpResult<Method> {
+    if method == Method::Get {
+        return Ok(method);
+    } else if method == Method::Post {
+        return Ok(match action {
+            "put" => Method::Put,
+            "post" => method,
+            _ => return err_stt!(?400),
+        });
+    }
+
+    err_stt!(?400)
+}
