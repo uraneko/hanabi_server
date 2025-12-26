b48e819a0ff9103a179528fae6a786b03a872435
uraneko <BOOTHAlessandra@tutanota.com>
2025-12-26 16:56:54 +0100

    add rebuilding and fetching the ui source code to the build script

--- build.rs
+++ build.rs
@@ -1,32 +1,87 @@
 use sqlite::Connection;
+use std::env::set_current_dir as cd;
+use std::process::{Command as Cmd, ExitStatus};
 use std::{fs, path::Path};
 
+#[derive(Debug)]
+enum Error {
+    DB(DBErr),
+    UI(UIErr),
+}
+
+impl From<DBErr> for Error {
+    fn from(err: DBErr) -> Self {
+        Self::DB(err)
+    }
+}
+
+impl From<UIErr> for Error {
+    fn from(err: UIErr) -> Self {
+        Self::UI(err)
+    }
+}
+
 fn main() -> Result<(), Error> {
+    check_db()?;
+    update_ui()?;
+
+    Ok(())
+}
+
+fn update_ui() -> Result<(), UIErr> {
+    change_dir()?;
+    build_ui()?;
+
+    copy_ui().map(|_| ())
+}
+
+#[derive(Debug)]
+enum UIErr {
+    FailedToChangeDir,
+    FailedToBuildPnpmPackage,
+    FailedToCopyFiles,
+}
+
+fn change_dir() -> Result<(), UIErr> {
+    cd("../../js/hanabi/hanabi").map_err(|_| UIErr::FailedToChangeDir)
+}
+
+fn build_ui() -> Result<ExitStatus, UIErr> {
+    Cmd::new("pnpm")
+        .arg("build")
+        .status()
+        .map_err(|_| UIErr::FailedToBuildPnpmPackage)
+}
+
+fn copy_ui() -> Result<ExitStatus, UIErr> {
+    Cmd::new("cp")
+        .args(&["dist", "-r", "../../../rust/hanabi"])
+        .status()
+        .map_err(|_| UIErr::FailedToCopyFiles)
+}
+
+fn check_db() -> Result<(), DBErr> {
     match check_dir() {
-        Err(Error::DataIsNotADir) => panic!("data already exists and is not a dir"),
-        Err(Error::DataDirNotFound) => make_dir()?,
+        Err(DBErr::DataIsNotADir) => panic!("data already exists and is not a dir"),
+        Err(DBErr::DataDirNotFound) => make_dir()?,
         Ok(()) => (),
         _ => unreachable!("function doesnt return this variant"),
     }
     let conn = open_database()?;
     match check_table(&conn) {
         Ok(()) => (),
-        Err(Error::FailedToProcessQueryRow) => panic!("internal error; db query processing failed"),
-        Err(Error::TableNotFound) => make_table(&conn)?,
+        Err(DBErr::FailedToProcessQueryRow) => {
+            panic!("internal error; db query processing failed")
+        }
+        Err(DBErr::TableNotFound) => make_table(&conn)?,
         _ => unreachable!("function doesnt return this variant"),
     }
 
-    check_columns(&conn)?;
-
-    Ok(())
+    check_columns(&conn)
 }
 
-const LOOKUP: &str = "PRAGMA table_info(users)";
-
-const CREATE: &str = "CREATE TABLE users (name TEXT, password TEXT);";
-
 #[derive(Debug)]
-enum Error {
+enum DBErr {
     FailedToCreateDataDir,
     FailedToOpenDB,
     FailedToProcessQueryRow,
@@ -37,53 +92,53 @@ enum Error {
     DataIsNotADir,
 }
 
-fn check_dir() -> Result<(), Error> {
+fn check_dir() -> Result<(), DBErr> {
     let path = Path::new("data");
     if path.is_file() || path.is_symlink() {
-        return Err(Error::DataIsNotADir);
+        return Err(DBErr::DataIsNotADir);
     } else if !path.exists() {
-        return Err(Error::DataDirNotFound);
+        return Err(DBErr::DataDirNotFound);
     }
 
     Ok(())
 }
 
-fn make_dir() -> Result<(), Error> {
-    fs::create_dir("data").map_err(|_| Error::FailedToCreateDataDir)
+fn make_dir() -> Result<(), DBErr> {
+    fs::create_dir("data").map_err(|_| DBErr::FailedToCreateDataDir)
 }
 
 // creates a new db if it doesnt exist
-fn open_database() -> Result<Connection, Error> {
-    sqlite::open("data/main.db3").map_err(|_| Error::FailedToOpenDB)
+fn open_database() -> Result<Connection, DBErr> {
+    sqlite::open("data/main.db3").map_err(|_| DBErr::FailedToOpenDB)
 }
 
-fn check_table(conn: &Connection) -> Result<(), Error> {
+fn check_table(conn: &Connection) -> Result<(), DBErr> {
     let mut stt = conn
         .prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
-        .map_err(|_| Error::FailedToProcessQueryRow)?;
-    stt.next().map_err(|_| Error::FailedToProcessQueryRow)?;
+        .map_err(|_| DBErr::FailedToProcessQueryRow)?;
+    stt.next().map_err(|_| DBErr::FailedToProcessQueryRow)?;
 
     match stt.read::<String, _>("name") {
         Ok(val) if val == "users".to_owned() => (),
-        _ => return Err(Error::TableNotFound),
+        _ => return Err(DBErr::TableNotFound),
     }
 
     Ok(())
 }
 
-fn make_table(conn: &Connection) -> Result<(), Error> {
+fn make_table(conn: &Connection) -> Result<(), DBErr> {
     conn.execute("CREATE TABLE users (name TEXT, password TEXT);")
-        .map_err(|_| Error::TableCreateFailed)
+        .map_err(|_| DBErr::TableCreateFailed)
 }
 
-fn check_columns(conn: &Connection) -> Result<(), Error> {
+fn check_columns(conn: &Connection) -> Result<(), DBErr> {
     let mut stt = conn
         .prepare("select * from users limit 0")
-        .map_err(|_| Error::FailedToProcessQueryRow)?;
-    stt.next().map_err(|_| Error::FailedToProcessQueryRow)?;
+        .map_err(|_| DBErr::FailedToProcessQueryRow)?;
+    stt.next().map_err(|_| DBErr::FailedToProcessQueryRow)?;
 
     if stt.column_names() != &["name".to_owned(), "password".into()] {
-        return Err(Error::TableColumnsMismatch);
+        return Err(DBErr::TableColumnsMismatch);
     }
 
     Ok(())
